bd987d9faecba966e713dddeb0b0120586ca958f
aakashdeveloper <ahanda205@gmail.com>
2022-07-22 07:31:46 +0530

    stringliteral

---
+++ javascript/3consolealert.js
@@ -0,0 +1,29 @@
+console.log() >>>>> for chekcing output as well as debugging
+alert() >>>> For notification
+confirm() >>> To check weather true of false
+prompt() >>>> to take any input form user
+
+alert('Form Submitted')
+alert('No Slot Left')
+
+confirm('Do you Want to leave')
+true
+confirm('Do you Want to leave')
+false
+var a = confirm('Do you Want to leave')
+undefined
+a
+true
+var a = confirm('Do you Want to leave')
+undefined
+a
+false
+prompt('What is your name')
+'Aakash'
+prompt('What is your age')
+'10'
+
+
+var a = prompt('Enter first value');
+var b = prompt('Enter second value');
+alert(Number(a)+Number(b))
---
+++ javascript/4operations.js
@@ -0,0 +1,86 @@
+= // assignment
+== // compare the data
+=== // compare the data & datatype
+
+var a = 10
+var b = "10" ///assignment
+
+a == b //compare the data
+true
+
+a === b // compare the data & datatype
+false
+
+var c = 10
+a === c
+true
+
+var a = 10
+var b = 20
+
+a == b
+false
+a === b
+false
+
+var a= 10
+var b = 10
+a === b
+true
+
+var a = 10
+var b = 21
+
+a>10
+false
+a>b
+false
+a<b
+true
+a>=b
+false
+a<=b
+true
+a!=b
+true
+a => b
+a => b
+
+//negation
+var a = true
+!a
+false
+
+var a = false
+!a
+true
+
+var a = 1
+!a
+
+var a = true
+!a
+false
+
+var a = false
+!a
+true
+
+var a = 1
+!a
+false
+
+var a = 0
+!a
+true
+
+var a = -10
+!a
+false
+
+var a = "hiii"
+!a
+false
+
+truthy > any number except 0 weather +ve or -ve, any string, true
+falsy> 0 , false, null, undefined
